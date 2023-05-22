929301ba2f64c3ca8eaa80647cc2a4a7b55150df
Nvdcloud <84348016+Nvdcloud@users.noreply.github.com>
2023-05-22 07:09:04 +0100

    Updating JS

--- catmeme.mjs
+++ catmeme.mjs
@@ -1,3 +1,25 @@
 import { catsData } from "./data.mjs";
+const emotionRadios = document.getElementById("emotion-radios")
 
-console.log(catsData)
+
+function getCatEmotions(){
+    const CAT_EMOTIONS = []
+    for (let cat of catsData){
+        for(let emotion of cat.emotionTags)
+            if(!CAT_EMOTIONS.includes(emotion)){
+            CAT_EMOTIONS.push(emotion)
+            }
+    }
+    return CAT_EMOTIONS
+}
+
+function renderRadios(){
+    let emotionOptions = getCatEmotions()
+    emotionRadios.innerHTML += `
+        <div class="radios">${emotionOptions}</div>
+    `
+
+}
+
+renderRadios()
+
